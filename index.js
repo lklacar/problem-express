3745e6c2e9ef474024d6afc7a3469730c997664e
Luka Klacar <lukaklacar@gmail.com>
2018-05-27 22:35:51 +0200

    Added some default http errors

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
+function problem(res) {
+    return function (title, type, status, detail, instance) {
+        res.setHeader("Content-Type", "application/problem+json");
+        res.send({
+            title,
+            type,
+            status,
+            detail,
+            instance
+        })
+    }
+}
+
 function init() {
     return function (req, res) {
-        req.problem = function (title, type, status, detail, instance) {
-            res.setHeader("Content-Type", "application/problem+json");
+        const problemRes = problem(res);
+        req.problem = {
+            new: problemRes,
 
-            res.send({
-                title,
-                type,
-                status,
-                detail,
-                instance
-            })
+            // 4xx
+            badRequest: problemRes("Bad request"),
+            unauthorized: problemRes('Unauthorized'),
+            paymentRequired: problemRes('Payment required'),
+            forbidden: problemRes('Forbidden'),
+            notFound: problemRes('Not found'),
+            methodNotAllowed: problemRes('Method not allowed'),
+            notAcceptable: problemRes('Not acceptable'),
+            proxyAuthRequired: problemRes('Proxy auth required'),
+            clientTimeout: problemRes('Client timeout'),
+            conflict: problemRes('Conflict'),
+            resourceGone: problemRes('Resource gone'),
+            lengthRequired: problemRes('Length required'),
+            preconditionFailed: problemRes('Preconditioning failed'),
+            entityTooLarge: problemRes('Entity too large'),
+            uriTooLong: problemRes('Uri too long'),
+            unsupportedMediaType: problemRes('Unsupported media type'),
+            rangeNotSatisfiable: problemRes('Range not satisfiable'),
+            expectationFailed: problemRes('Expectation failed'),
+            teapot: problemRes('Teapot'),
+            badData: problemRes('Bad data'),
+            locked: problemRes('Locked'),
+            failedDependency: problemRes('Failed dependency'),
+            preconditionRequired: problemRes('Precondition required'),
+            tooManyRequests: problemRes('Too many requires'),
+            illegal: problemRes('Illegal'),
+
+            //5xx
+            badImplementation: problemRes('Bad implementation'),
+            notImplemented: problemRes('Not implemented'),
+            badGateway: problemRes('Bad gateway'),
+            serverUnavailable: problemRes('Server unavailable'),
+            gatewayTimeout: problemRes('Gateway timeout'),
         }
     }
 }
