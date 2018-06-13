e6a26aeacbedc359d0b48032898630f56a3bf83b
Luka Klacar <l.klacar@vegaitsourcing.rs>
2018-06-13 16:08:15 +0200

    Added status codes

--- index.js
+++ index.js
@@ -1,58 +1,67 @@
-function problem(res) {
-    return function (title, type, status, detail, instance) {
-        res.setHeader("Content-Type", "application/problem+json");
-        res.send({
-            title,
-            type,
-            status,
-            detail,
-            instance
-        })
+function clean(obj) {
+  const propNames = Object.getOwnPropertyNames(obj);
+  for (var i = 0; i < propNames.length; i++) {
+    const propName = propNames[i];
+    if (obj[propName] === null || obj[propName] === undefined) {
+      delete obj[propName];
     }
+  }
+}
+
+function problem(res) {
+  return function (status, title, type, detail, instance) {
+    res.setHeader("Content-Type", "application/problem+json");
+    res.send(clean({
+      title,
+      type,
+      status,
+      detail,
+      instance
+    }))
+  }
 }
 
 function init() {
-    return function (req, res) {
-        const problemRes = problem(res);
-        req.problem = {
-            new: problemRes,
+  return function (req, res) {
+    const problemRes = problem(res);
+    req.problem = {
+      new: problemRes,
 
-            // 4xx
-            badRequest: problemRes("Bad request"),
-            unauthorized: problemRes('Unauthorized'),
-            paymentRequired: problemRes('Payment required'),
-            forbidden: problemRes('Forbidden'),
-            notFound: problemRes('Not found'),
-            methodNotAllowed: problemRes('Method not allowed'),
-            notAcceptable: problemRes('Not acceptable'),
-            proxyAuthRequired: problemRes('Proxy auth required'),
-            clientTimeout: problemRes('Client timeout'),
-            conflict: problemRes('Conflict'),
-            resourceGone: problemRes('Resource gone'),
-            lengthRequired: problemRes('Length required'),
-            preconditionFailed: problemRes('Preconditioning failed'),
-            entityTooLarge: problemRes('Entity too large'),
-            uriTooLong: problemRes('Uri too long'),
-            unsupportedMediaType: problemRes('Unsupported media type'),
-            rangeNotSatisfiable: problemRes('Range not satisfiable'),
-            expectationFailed: problemRes('Expectation failed'),
-            teapot: problemRes('Teapot'),
-            badData: problemRes('Bad data'),
-            locked: problemRes('Locked'),
-            failedDependency: problemRes('Failed dependency'),
-            preconditionRequired: problemRes('Precondition required'),
-            tooManyRequests: problemRes('Too many requires'),
-            illegal: problemRes('Illegal'),
+      // 4xx
+      badRequest: problemRes(400, "Bad request"),
+      unauthorized: problemRes(401, 'Unauthorized'),
+      paymentRequired: problemRes(402, 'Payment required'),
+      forbidden: problemRes(403, 'Forbidden'),
+      notFound: problemRes(404, 'Not found'),
+      methodNotAllowed: problemRes(405, 'Method not allowed'),
+      notAcceptable: problemRes(406, 'Not acceptable'),
+      proxyAuthRequired: problemRes(407, 'Proxy auth required'),
+      clientTimeout: problemRes(408, 'Client timeout'),
+      conflict: problemRes(409, 'Conflict'),
+      resourceGone: problemRes(410, 'Resource gone'),
+      lengthRequired: problemRes(411, 'Length required'),
+      preconditionFailed: problemRes(412, 'Preconditioning failed'),
+      entityTooLarge: problemRes(413, 'Entity too large'),
+      uriTooLong: problemRes(414, 'Uri too long'),
+      unsupportedMediaType: problemRes(415, 'Unsupported media type'),
+      rangeNotSatisfiable: problemRes(416, 'Range not satisfiable'),
+      expectationFailed: problemRes(417, 'Expectation failed'),
+      teapot: problemRes(418, 'Teapot'),
+      badData: problemRes(422, 'Bad data'),
+      locked: problemRes(423, 'Locked'),
+      failedDependency: problemRes(424, 'Failed dependency'),
+      preconditionRequired: problemRes(428, 'Precondition required'),
+      tooManyRequests: problemRes(429, 'Too many requests'),
+      illegal: problemRes(451, 'Illegal'),
 
-            //5xx
-            badImplementation: problemRes('Bad implementation'),
-            notImplemented: problemRes('Not implemented'),
-            badGateway: problemRes('Bad gateway'),
-            serverUnavailable: problemRes('Server unavailable'),
-            gatewayTimeout: problemRes('Gateway timeout'),
-        }
+      //5xx
+      badImplementation: problemRes(500, 'Bad implementation'),
+      notImplemented: problemRes(501, 'Not implemented'),
+      badGateway: problemRes(502, 'Bad gateway'),
+      serverUnavailable: problemRes(503, 'Server unavailable'),
+      gatewayTimeout: problemRes(504, 'Gateway timeout'),
     }
+  }
 }
-
 
 module.exports = init;
