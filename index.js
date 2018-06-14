0daa82528214365fe875efff36057ea2bfa78e34
Luka Klacar <l.klacar@vegaitsourcing.rs>
2018-06-14 15:28:11 +0200

    Added support for custom problems

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function clean(obj) {
   const propNames = Object.getOwnPropertyNames(obj);
-  for (let i = 0; i < propNames.length; i++) {
+  for (var i = 0; i < propNames.length; i++) {
     const propName = propNames[i];
     if (obj[propName] === null || obj[propName] === undefined) {
       delete obj[propName];
@@ -8,64 +8,32 @@ function clean(obj) {
   }
 }
 
-function problem(res) {
-  return function (status, title, type, detail, instance) {
-    return function () {
-      res.setHeader('Content-Type', 'application/problem+json');
-      res.status(status);
-      const result = {
-        title,
-        type,
-        status,
-        detail,
-        instance
-      };
-      clean(result);
-      res.send(result);
-    };
+function sendRequest(res, args) {
+  res.setHeader('Content-Type', 'application/problem+json');
+  res.status(args.statusCode);
+  const result = {
+    ...args
+  };
+  clean(result);
+  res.send(result);
+};
+
+function registerProblem(key, handler, res) {
+  const problem = handler;
+
+  res.problem[key] = function (args) {
+    const problemResult = problem(args);
+    sendRequest(res, problemResult);
   };
 }
 
-function init() {
+function init(additionalProbems) {
   return function (req, res, next) {
-    const problemRes = problem(res);
-    res.problem = {
-      new: problemRes,
-
-      // 4xx
-      badRequest: problemRes(400, 'Bad request'),
-      unauthorized: problemRes(401, 'Unauthorized'),
-      paymentRequired: problemRes(402, 'Payment required'),
-      forbidden: problemRes(403, 'Forbidden'),
-      notFound: problemRes(404, 'Not found'),
-      methodNotAllowed: problemRes(405, 'Method not allowed'),
-      notAcceptable: problemRes(406, 'Not acceptable'),
-      proxyAuthRequired: problemRes(407, 'Proxy auth required'),
-      clientTimeout: problemRes(408, 'Client timeout'),
-      conflict: problemRes(409, 'Conflict'),
-      resourceGone: problemRes(410, 'Resource gone'),
-      lengthRequired: problemRes(411, 'Length required'),
-      preconditionFailed: problemRes(412, 'Preconditioning failed'),
-      entityTooLarge: problemRes(413, 'Entity too large'),
-      uriTooLong: problemRes(414, 'Uri too long'),
-      unsupportedMediaType: problemRes(415, 'Unsupported media type'),
-      rangeNotSatisfiable: problemRes(416, 'Range not satisfiable'),
-      expectationFailed: problemRes(417, 'Expectation failed'),
-      teapot: problemRes(418, 'Teapot'),
-      badData: problemRes(422, 'Bad data'),
-      locked: problemRes(423, 'Locked'),
-      failedDependency: problemRes(424, 'Failed dependency'),
-      preconditionRequired: problemRes(428, 'Precondition required'),
-      tooManyRequests: problemRes(429, 'Too many requests'),
-      illegal: problemRes(451, 'Illegal'),
-
-      //5xx
-      badImplementation: problemRes(500, 'Bad implementation'),
-      notImplemented: problemRes(501, 'Not implemented'),
-      badGateway: problemRes(502, 'Bad gateway'),
-      serverUnavailable: problemRes(503, 'Server unavailable'),
-      gatewayTimeout: problemRes(504, 'Gateway timeout'),
-    };
+    res.problem = {};
+    Object.keys(additionalProbems).forEach(problemKey => {
+      const problem = additionalProbems[problemKey];
+      registerProblem(problemKey, problem, res);
+    });
     next();
   };
 }
